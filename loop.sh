a4fd16deba56ead5ac5fdc4f098d851294d4a04c
SAY-droid427 <sayanimallick0218@gmail.com>
2021-04-27 11:41:50 +0530

    Filtered

--- loop.sh
+++ loop.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
 function loop1(){
-    
+    NAMES="Percy Annabeth Grover Chiron"
+    for NAME in $NAMES
+    do
+       echo "Welcome to Camp Half-Blood, $NAME"
+    done
 }
 
+loop1
